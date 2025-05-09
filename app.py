b9a783d0cb0bef2af4552ddb02d9f08246440298
Eltaf-azizi <iltfhuss@gmail.com>
2025-05-09 19:21:03 +0500

    working on the load sentiment model

--- app.py
+++ app.py
@@ -16,3 +16,25 @@ SEARCH_ENGINE_ID = os.environ.get("SEARCH_ENGINE_ID")
 
 # Sentiment pipeline
 SENTIMENT_MODEL = None
+
+
+RESULTS_DIR = os.path.join(os.getcwd(), "analysis_results")
+os.makedirs(RESULTS_DIR, exit_ok = True)
+
+
+PDF_DIR = os.path.join(os.getcwd(), "pdf_reports")
+os.markdirs(PDF_DIR, exit_ok=True)
+
+
+def load_sentiment_model():
+    global SENTIMENT_MODEL
+    if SENTIMENT_MODEL is None:
+        SENTIMENT_MODEL = pipeline("sentiment-analysis", model="")
+    return SENTIMENT_MODEL
+
+
+
+
+@app.route('/analyze', methods=['POST'])
+def analyze():
+    
